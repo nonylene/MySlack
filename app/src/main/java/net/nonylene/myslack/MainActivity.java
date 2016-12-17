28b285c3bb32aef707a3fa287e359bf83c589647
nonylene <cookie.speaker@gmail.com>
2016-12-17 18:56:44 +0900

    onclick to toast

--- app/src/main/java/net/nonylene/myslack/MainActivity.java
+++ app/src/main/java/net/nonylene/myslack/MainActivity.java
@@ -2,12 +2,34 @@ package net.nonylene.myslack;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // setContentView 前に行うと View が表示されていないのでエラー
         setContentView(R.layout.activity_main);
+        EditText editText = (EditText) findViewById(R.id.post_edit);
+        Button submitButton = (Button) findViewById(R.id.post_button);
+
+//        editText.setHint("KMC");
+//        editText.setVisibility(View.GONE);
+//        // pixel
+//        editText.setWidth(300);
+
+        submitButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(v.getContext(), "clicked!", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+
     }
 }
