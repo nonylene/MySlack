c779829e1a8e2b0ec56de387de639d99c0422a7c
nonylene <cookie.speaker@gmail.com>
2017-01-07 16:35:28 +0900

    example fragment

--- app/src/main/java/net/nonylene/myslack/Main2Activity.java
+++ app/src/main/java/net/nonylene/myslack/Main2Activity.java
@@ -1,6 +1,7 @@
 package net.nonylene.myslack;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 public class Main2Activity extends AppCompatActivity {
@@ -9,5 +10,9 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(android.R.id.content, new ExampleFragment());
+        fragmentTransaction.commit();
     }
 }
