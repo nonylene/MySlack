db52ad822f10d5ad41975f21bb3d2d953b3f3c33
nonylene <cookie.speaker@gmail.com>
2017-01-07 16:34:32 +0900

    preference

--- app/src/main/java/net/nonylene/myslack/MainActivity.java
+++ app/src/main/java/net/nonylene/myslack/MainActivity.java
@@ -1,6 +1,8 @@
 package net.nonylene.myslack;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -31,5 +33,15 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
 
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreferences.edit()
+                .putInt("hoge", 1)
+                .putString("piyo", "piyoipyo")
+                .putBoolean("foo", true)
+                .apply();
+
+        sharedPreferences.getInt("hoge", 0);
+        sharedPreferences.getString("piyo", null);
+        sharedPreferences.getBoolean("foo", false);
     }
 }
